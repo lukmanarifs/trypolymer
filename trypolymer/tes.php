adf7f68d45fd534535dd16a18eabf8414aa6fa8e
zhed <lukman.ars@gmail.com>
2015-09-04 22:08:24 +0700

    preparation for polymer demo

---
+++ trypolymer/tes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+    <link rel="import" href="dreams-element.html">
+  </head>
+  <body>
+    <dreams-element></dreams-element>
+    <hr>
+    <dom-element></dom-element>
+    <hr>
+    <picture-frame>
+      <img src="dreams-logo.jpg">
+    </picture-frame>
+    <hr>
+    <name-tag></name-tag>
+    <hr>
+    <configurable-name-tag apps="VoipGraph"></configurable-name-tag>
+    <hr>
+    <editable-apps-tag></editable-apps-tag>
+
+    </body>
+</html>
